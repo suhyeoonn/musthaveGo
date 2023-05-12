c425e02a50f4780f692818563ddaaa28f8f53abe
suhyeoonn <suhyeon1740@gmail.com>
2023-05-13 02:11:03 +0900

    ✨ 웹서버 실습

---
+++ helloServer/helloServer_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestIndexHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/", nil)
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := io.ReadAll(res.Body)
+	assert.Equal("Hello World", string(data))
+}
+
+func TestBarHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar?id=1&name=a", nil)
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := io.ReadAll(res.Body)
+	assert.Equal("Hello a! id:1", string(data))
+}
+
+func TestStudentHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/student", nil)
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	student := new(Student)
+	err := json.NewDecoder(res.Body).Decode(student)
+	assert.Nil(err)
+	assert.Equal("a", student.Name)
+	assert.Equal(16, student.Age)
+}
---
+++ helloServer/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type Student struct {
+	Name  string
+	Age   int
+	Score int
+}
+
+func StudentHandler(w http.ResponseWriter, r *http.Request) {
+	var student = Student{"a", 16, 87}
+	data, _ := json.Marshal(student)
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, string(data))
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	name := values.Get("name")
+	if name == "" {
+		name = "World"
+	}
+	id, _ := strconv.Atoi(values.Get("id"))
+	fmt.Fprintf(w, "Hello %s! id:%d", name, id)
+}
+
+func MakeWebHandler() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	//mux.Handle("/", http.FileServer(http.Dir("static")))
+	mux.HandleFunc("/bar", barHandler)
+	mux.HandleFunc("/student", StudentHandler)
+
+	return mux
+}
+
+func main() {
+	http.ListenAndServe(":3000", MakeWebHandler())
+
+	//http.Handle("/", http.FileServer(http.Dir("static")))
+	//http.ListenAndServe(":3000", nil)
+}
